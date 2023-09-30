2afae43dfbfc5cfc43855cc7a8a6b53368059fb7
Robin Chew <me@robin.com.au>
2023-10-01 01:29:54 +0800

    Value of WS_PORT environment variable needs to be converted to integer.

--- erlchat/src/erlchat_app.erl
+++ erlchat/src/erlchat_app.erl
@@ -12,7 +12,7 @@ start(_Type, _Args) ->
     ]),
     {ok, _} = cowboy:start_clear(
         server_listener,
-        [{port, env:get("WS_PORT")}],
+        [{port, list_to_integer(env:get("WS_PORT"))}],
         #{env => #{dispatch => Dispatch}}),
 	erlchat_sup:start_link().
 
