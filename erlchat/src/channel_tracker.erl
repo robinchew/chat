25c97e9ab64d998b6d5cf38ae95c188981e855c0
Robin Chew <me@robin.com.au>
2023-10-13 01:41:14 +0800

    Cleanup channel_tracker, because most methods are now in channel_spawn.

--- erlchat/src/channel_tracker.erl
+++ erlchat/src/channel_tracker.erl
@@ -5,9 +5,7 @@
     start_link/0,
     get_pid/1,
     subscribe/2,
-    unsubscribe/2,
-    notify_subscribers/2,
-    notify_subscribers/3,
+    unsubscribe/1,
     init/1,
     handle_cast/2,
     handle_call/3
@@ -16,13 +14,6 @@
 -include_lib("kernel/include/logger.hrl").
 
 % Functions
-
-notify_ws_pids([WsPid | OtherWsPids], Msg) ->
-    WsPid ! {refresh, Msg},
-    notify_ws_pids(OtherWsPids, Msg);
-
-notify_ws_pids([], _Msg) ->
-    done.
 
 % Public
 
@@ -35,14 +26,8 @@ get_pid(ChannelUuid) ->
 subscribe(ChannelUuid, Pid) ->
     gen_server:cast(?MODULE, {subscribe, ChannelUuid, Pid}).
 
-unsubscribe(ChannelUuid, Pid) ->
-    gen_server:cast(?MODULE, {unsubscribe, ChannelUuid, Pid}).
-
-notify_subscribers(ChannelUuid, Msg) ->
-    gen_server:cast(?MODULE, {notify_subscribers, ChannelUuid, Msg}).
-
-notify_subscribers(NotifierWsPid, ChannelUuid, Msg) when is_pid(NotifierWsPid) ->
-    gen_server:cast(?MODULE, {notify_subscribers, ChannelUuid, NotifierWsPid, Msg}).
+unsubscribe(ChannelUuid) ->
+    gen_server:cast(?MODULE, {unsubscribe, ChannelUuid}).
 
 
 % Private
@@ -51,46 +36,18 @@ init(_Arg) ->
     {ok, #{channel_map => #{}}}.
 
 handle_cast({subscribe, ChannelUuid, Pid}, State) ->
-    WsPidList = nested:get([channel_map, ChannelUuid, ws_pid_list], State, []),
     {noreply, nested:put(
-        [channel_map, ChannelUuid, ws_pid_list],
-        WsPidList ++ [Pid],
+        [channel_map, ChannelUuid],
+        Pid,
         State)};
 
-handle_cast({unsubscribe, ChannelUuid, Pid}, State) ->
-    WsPidList = nested:get([channel_map, ChannelUuid, ws_pid_list], State, no_list),
-
-    NewState = case WsPidList of
-        no_list -> State;
-        List -> nested:update(
-            [channel_map, ChannelUuid, ws_pid_list],
-            lists:delete(Pid, List),
-            State)
-    end,
-
-    {noreply, NewState};
-
-handle_cast({notify_subscribers, ChannelUuid, Msg}, State) ->
-    WsPidList = nested:get([channel_map, ChannelUuid, ws_pid_list], State, []),
-    notify_ws_pids(WsPidList, Msg),
-    {noreply, State};
-
-handle_cast({notify_subscribers, ChannelUuid, NotifierWsPid, Msg}, State) when is_pid(NotifierWsPid) ->
-    WsPidList = nested:get([channel_map, ChannelUuid, ws_pid_list], State, []),
-
-    notify_ws_pids(
-        % Only other subscribers are notified
-        % not the one who inititiated it.
-        [WsPid || WsPid <- WsPidList, WsPid =/= NotifierWsPid],
-        Msg),
-    {noreply, State}.
+handle_cast({unsubscribe, ChannelUuid}, State) ->
+    {noreply, nested:remove(
+        [channel_map, ChannelUuid],
+        State)}.
 
 handle_call({get_pid, ChannelUuid}, _From, State) ->
-    Return = case nested:get([channel_map, ChannelUuid, ws_pid_list], State, no_pid) of
-        no_pid -> no_pid;
-        [Pid] -> Pid
-    end,
-    {reply, Return, State};
+    {reply, nested:get([channel_map, ChannelUuid], State, no_pid), State};
 
 handle_call(_, _From, State) ->
     {reply, nothing, State}.
